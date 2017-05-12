df0c0a05946c7e78d137ab5e6a2121e8dbf1f5f9
ShirataHikaru <dendenden00@gmail.com>
2017-05-12 11:57:14 +0900

    ノー黄金比

---
+++ script.js
@@ -0,0 +1,36 @@
+function slideInLeft() {
+  $('#animeA').css({
+    'opacity' : '1',
+  })
+  .animate({'right':'2%'}, 1800);
+
+}
+
+function slideInRight() {
+  $('#animeB').css({
+    'opacity' : '1',
+  })
+  .animate({'left':'2%'}, 1800);
+}
+
+
+$(function() {
+  $('.stageBase').css({'height':$(window).height()});
+  $.scrollify({
+    section:'.stageBase',
+    scrollSpeed: 1000,
+    before:function(i,panels){
+      var ref = panels[i].attr('id');
+      if(ref == 'stage2'){
+        slideInLeft();
+        slideInRight();
+      }
+    },
+    after:function(i,panels){
+      var ref = panels[i].attr('id');
+        $('#animeA').css('right','100%');
+        $('#animeB').css('left','100%');
+    }
+  });
+  $("#animeA, #animeB").css("opacity", "0");
+});
